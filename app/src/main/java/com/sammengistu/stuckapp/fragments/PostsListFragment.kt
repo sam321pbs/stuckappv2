7e1d1f23f00ef5196b731e6c87fc7472c54e36b4
Samuel Mengistu <sam321pbs@gmail.com>
2019-08-20 14:20:34 -0400

    start of post views

---
+++ app/src/main/java/com/sammengistu/stuckapp/fragments/PostsListFragment.kt
@@ -0,0 +1,13 @@
+package com.sammengistu.stuckapp.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class PostsListFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/sammengistu/stuckapp/views/IconToCountView.kt
@@ -0,0 +1,47 @@
+package com.sammengistu.stuckapp.views
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.sammengistu.stuckapp.R
+
+class IconToCountView(context: Context, attrs: AttributeSet) : LinearLayout(context) {
+    var iconView: ImageView = ImageView(context)
+    var countView: TextView = TextView(context)
+
+    init {
+        buildView()
+        context.theme.obtainStyledAttributes(
+            attrs,
+            R.styleable.IconToCountView,
+            0, 0).apply {
+
+            try {
+                val iconId = getResourceId(R.styleable.IconToCountView_iconSrc, R.mipmap.peter_griffan_head_shot)
+                var textCount = getString(R.styleable.IconToCountView_textCount, 0)
+                iconView.setImageDrawable(context.getDrawable(iconId))
+                countView.text = textCount
+            } finally {
+                recycle()
+            }
+        }
+    }
+
+    fun setCount(countTotal: Int) {
+        countView.text = countTotal.toString()
+    }
+
+    private fun buildView() {
+        setPadding(10, 10, 10, 10)
+        orientation = HORIZONTAL
+
+        val params = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
+        params.marginEnd = 10
+
+        iconView.layoutParams = params
+        addView(iconView)
+        addView(countView)
+    }
+}
