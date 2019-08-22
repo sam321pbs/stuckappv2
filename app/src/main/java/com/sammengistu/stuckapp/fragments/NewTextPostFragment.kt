abc2a81f2c9d992cae2d0dd9f2d8b9b6427d8c9e
Samuel Mengistu <sam321pbs@gmail.com>
2019-08-22 13:47:13 -0400

    add new choices view to new post

--- app/src/main/java/com/sammengistu/stuckapp/fragments/NewTextPostFragment.kt
+++ app/src/main/java/com/sammengistu/stuckapp/fragments/NewTextPostFragment.kt
@@ -1,9 +1,33 @@
 package com.sammengistu.stuckapp.fragments
 
-import com.sammengistu.stuckapp.R
+import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
+import com.sammengistu.stuckapp.views.ChoiceCardView
+import kotlinx.android.synthetic.main.fragment_new_post_text.*
+
 
 class NewTextPostFragment : BaseFragment() {
+
+    lateinit var mChoicesContainer : LinearLayout
+
     override fun getLayoutId(): Int {
-        return R.layout.fragment_new_post_text
+        return com.sammengistu.stuckapp.R.layout.fragment_new_post_text
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mChoicesContainer = choices_container
+        add_choice_fab.setOnClickListener {
+            if (mChoicesContainer.childCount < 4) {
+                val newChild = ChoiceCardView(this@NewTextPostFragment.activity!!.applicationContext)
+                newChild.setHint("Choice ${mChoicesContainer.childCount + 1}")
+                mChoicesContainer.addView(newChild)
+
+                if (mChoicesContainer.childCount >= 4) {
+                    add_choice_fab.visibility = View.GONE
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/sammengistu/stuckapp/views/ChoiceCardView.kt
+++ app/src/main/java/com/sammengistu/stuckapp/views/ChoiceCardView.kt
@@ -2,8 +2,87 @@ package com.sammengistu.stuckapp.views
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.LinearLayout
+import androidx.appcompat.view.ContextThemeWrapper
 import androidx.cardview.widget.CardView
+import com.sammengistu.stuckapp.R
 
-class ChoiceCardView(context: Context, attrs: AttributeSet) : CardView(context) {
 
+class ChoiceCardView(context: Context, attrs: AttributeSet?) :
+    CardView(ContextThemeWrapper(context, R.style.new_post_choice), attrs) {
+
+    private val choiceEditText: EditText = EditText(context)
+
+    init {
+        if (attrs == null) {
+            applyStyleManually(context)
+        } else {
+            context.theme.obtainStyledAttributes(
+                attrs,
+                R.styleable.ChoiceCardView,
+                0, 0
+            ).apply {
+                try {
+                    choiceEditText.hint =
+                        getString(R.styleable.ChoiceCardView_hintText)
+                } finally {
+                    recycle()
+                }
+            }
+        }
+        buildEditTextView()
+    }
+
+    constructor(context: Context) : this(context, null)
+
+    private fun applyStyleManually(context: Context) {
+        val attrs =
+            intArrayOf(
+                android.R.attr.layout_width, android.R.attr.layout_height,
+                android.R.attr.layout_marginTop, android.R.attr.layout_marginStart,
+                android.R.attr.layout_marginEnd
+            )
+        val attrChoices =
+            context.theme.obtainStyledAttributes(
+                R.style.new_post_choice,
+                attrs
+            )
+
+        val params = LinearLayout.LayoutParams(
+            attrChoices.getLayoutDimension(
+                attrChoices.getIndex(0),
+                ViewGroup.LayoutParams.MATCH_PARENT
+            ),
+            attrChoices.getLayoutDimension(
+                attrChoices.getIndex(1),
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+        )
+
+        val marginTop = attrChoices.getDimension(attrChoices.getIndex(2), 10f)
+        val marginStart = attrChoices.getDimension(attrChoices.getIndex(3), 10f)
+        val marginEnd = attrChoices.getDimension(attrChoices.getIndex(4), 10f)
+
+        layoutParams = params
+
+        params.topMargin = marginTop.toInt()
+        params.marginStart = marginStart.toInt()
+        params.marginEnd = marginEnd.toInt()
+    }
+
+    fun setHint(hint: String) {
+        choiceEditText.hint = hint
+    }
+
+    private fun buildEditTextView() {
+        val params = ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+        setPadding(5, 5, 5, 5)
+        choiceEditText.layoutParams = params
+        addView(choiceEditText)
+    }
 }
