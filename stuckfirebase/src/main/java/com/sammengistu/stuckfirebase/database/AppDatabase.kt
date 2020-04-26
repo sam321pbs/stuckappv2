b41356fb06b25d83b7fce8def0afd94cba3e8fb4
Samuel Mengistu <sam321pbs@gmail.com>
2020-04-26 12:31:59 -0400

    Recreates all tables on db upgrade

--- stuckfirebase/src/main/java/com/sammengistu/stuckfirebase/database/AppDatabase.kt
+++ stuckfirebase/src/main/java/com/sammengistu/stuckfirebase/database/AppDatabase.kt
@@ -15,7 +15,7 @@ import com.sammengistu.stuckfirebase.models.PostModel
 import com.sammengistu.stuckfirebase.models.UserModel
 
 @Database(entities = [PostModel::class, HiddenItemModel::class, UserModel::class],
-    version = 9, exportSchema = false)
+    version = 10, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun postsDao(): PostDao
     abstract fun hiddenItemsDao(): HiddenItemsDao
@@ -33,6 +33,17 @@ abstract class AppDatabase : RoomDatabase() {
         }
 
         private fun buildDatabase(context: Context): AppDatabase {
+            val MIGRATION_8_10 = object : Migration(8, 10) {
+                override fun migrate(database: SupportSQLiteDatabase) {
+                    database.execSQL("DROP TABLE posts")
+                    database.execSQL("DROP TABLE hidden_items")
+                    database.execSQL("DROP TABLE users")
+
+                    database.execSQL("CREATE TABLE IF NOT EXISTS `posts` (`_id` INTEGER PRIMARY KEY AUTOINCREMENT, `ownerRef` TEXT NOT NULL, `question` TEXT NOT NULL, `privacy` TEXT NOT NULL, `category` TEXT NOT NULL, `type` TEXT NOT NULL, `image1` TEXT NOT NULL, `image2` TEXT NOT NULL, `choice1` TEXT NOT NULL, `choice2` TEXT NOT NULL, `choice3` TEXT NOT NULL, `choice4` TEXT NOT NULL, `votes1` INTEGER NOT NULL, `votes2` INTEGER NOT NULL, `votes3` INTEGER NOT NULL, `votes4` INTEGER NOT NULL, `totalStars` INTEGER NOT NULL, `totalComments` INTEGER NOT NULL, `ref` TEXT NOT NULL)")
+                    database.execSQL("CREATE TABLE IF NOT EXISTS `hidden_items` (`_id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `ownerRef` TEXT NOT NULL, `itemRef` TEXT NOT NULL, `type` TEXT NOT NULL)")
+                    database.execSQL("CREATE TABLE IF NOT EXISTS `users` (`_id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `userId` TEXT NOT NULL, `username` TEXT NOT NULL, `avatar` TEXT NOT NULL, `name` TEXT NOT NULL, `occupation` TEXT NOT NULL, `education` TEXT NOT NULL, `bio` TEXT NOT NULL, `ageGroup` INTEGER NOT NULL, `gender` INTEGER NOT NULL, `totalMadeVotes` INTEGER NOT NULL, `totalReceivedVotes` INTEGER NOT NULL, `totalReceivedStars` INTEGER NOT NULL, `ref` TEXT NOT NULL)")
+                }
+            }
 
             val MIGRATION_7_8 = object : Migration(7, 8) {
                 override fun migrate(database: SupportSQLiteDatabase) {
@@ -80,7 +91,7 @@ abstract class AppDatabase : RoomDatabase() {
                 AppDatabase::class.java,
                 DATABASE_NAME
             )
-                .addMigrations(MIGRATION_2_3, MIGRATION_3_4, MIGRATION_6_7, MIGRATION_7_8)
+                .addMigrations(MIGRATION_2_3, MIGRATION_3_4, MIGRATION_6_7, MIGRATION_7_8, MIGRATION_8_10)
                 .fallbackToDestructiveMigration().build()
         }
     }
